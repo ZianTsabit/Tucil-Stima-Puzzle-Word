5cf246117f465a1d6bc27a245ed25dbed5361ef8
ghaziankamil <16520465@std.stei.itb.ac.id>
2022-01-20 12:57:01 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -18,18 +18,20 @@ int N = 0; // jumlah baris
 int M = 0; // jumlah kolom 
 
 unordered_set<string> wordlist;		// unordered list untuk menyimpan kata yang ingin dicari	
-vector<vector<char>> puzzle;       // array dua dimensi untuk menyimpan puzzle
+vector<vector<char>> puzzle;        // array dua dimensi untuk menyimpan puzzle
+vector<vector<char>> jawaban;      
 set<string> words;
 
 void bacaDaftarKata(string filename);
 void bacaPuzzle(string filename);
 void cariKata();
-void printHasil();
+void printKata(string filename);
 
 
 void printPuzzles();
 void printDaftarKata();
 
+// FUNGSI UTAMA PROGRAM
 
 int main(){
     cout<<"***********SELAMAT DATANG DI WORD SEARCH PUZZLE GAME***********" <<endl;
@@ -48,10 +50,17 @@ int main(){
     
     printDaftarKata();
 
-    cout<<endl;
     cout<<"Hasil...";
+    
+    
+    cout<<endl;
+
+    cariKata();
+    printKata("../test/answers.txt");
 
 }
+
+// FUNGSI UNTUK MEMBACA PUZZLE DARI INPUT FILE
 
 void bacaPuzzle(string filename){
     ifstream pzlReader(filename);
@@ -81,6 +90,8 @@ void bacaPuzzle(string filename){
     }
 }
 
+// FUNGSI UNTUK MENAMPILKAN PUZZLE KE LAYAR
+
 void printPuzzles(){
     
     for (int i=0; i< N; i++){
@@ -105,6 +116,9 @@ void printPuzzles(){
 //     return (count >= minLength) && (count <= maxLength);
 // }
 
+
+// FUNGSI UNTUK MEMBACA DAFTAR KATA YANG HARUS DICARI DARI INPUT FILE
+
 void bacaDaftarKata(string filename){
     ifstream ktReader(filename);
 
@@ -123,10 +137,81 @@ void bacaDaftarKata(string filename){
     }
 }
 
+// FUNGSI UNTUK MENAMPILKAN DAFTAR KATA YANG HARUS DICARI KE LAYAR
+
 void printDaftarKata(){
     unordered_set<string> :: iterator itr;
     for (itr = wordlist.begin(); itr != wordlist.end(); itr++){
         cout<< (*itr) <<endl;
     }
     cout << endl;
+}
+
+// FUNGSI MIN ANTAR DUA INTEGER
+
+int min(int a, int b){
+    if (a < b){
+        return a;
+    }
+    return b;
+}
+
+// FUNGSI UNTUK MEMERIKSA APAKAH SEBUAH KATA TERDAPAT PADA DAFTAR KATA
+
+void checkWord(string s){
+    
+    if (wordlist.count(s)){
+        words.insert(s);
+    }
+}
+
+
+// FUNGSI UNTUK MENCARI KATA PADA PUZZLE SECARA HORIZONTAL
+
+void cariHorizontal(int r, int c){
+
+    string test1 = "";
+    string test2 = "";
+
+    for (int i= 0; i < min(M-c,maxLength); i++){
+        test1 = test1 + puzzle[r][c+i];
+        test2 = puzzle[r][c+1] + test2;
+        if ((i+1) >= minLength){
+            checkWord(test1);
+            checkWord(test2);
+        }
+    }
+}
+
+// FUNGSI UNTUK MENCARI KATA
+
+void cariKata(){
+    for (int r = 0; r < N; r++){
+        for (int c = 0; c < M; c++){
+            cariHorizontal(r, c);
+        }
+    }
+}
+
+// FUNGSI UNTUK MENAMPILKAN JAWABAN KE LAYAR
+
+void printKata(string filename){
+    ofstream wordprinter (filename);
+    if (wordprinter.is_open()){
+        for(auto const& i: words){
+            wordprinter << i  << endl;
+        }
+        wordprinter.close();
+    }
+}
+
+// BRUTE FORCE ALGORITHM
+
+void searchHorizontal(string s){
+    
+    
+    for(int i = 0; i <= M; i++){
+        int j;
+
+    }
 }
